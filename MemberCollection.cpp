e62486f308104d0f23bba90422befcf4213a4564
wnsgud245 <44499629+wnsgud245@users.noreply.github.com>
2019-05-31 16:01:19 +0900

    Update MemberCollection.cpp

--- MemberCollection.cpp
+++ MemberCollection.cpp
@@ -2,7 +2,7 @@
 
 void MemberCollection::createMember(std::string id, std::string passwd, std::string name, std::string ssn, std::string user_type)
 {
-	memberList.push_back(Member(id, passwd, name, ssn, user_type)); // Member¿¡¼­ ÁÖ¼®Ã³¸®ÇÑ ¸â¹öº¯¼ö´Â ÃßÈÄ¿¡ Ãß°¡ÇÏ´Â ¹æÇâÀ¸·Î
+	memberList.push_back(Member(id, passwd, name, ssn, user_type)); // Memberì—ì„œ ì£¼ì„ì²˜ë¦¬í•œ ë©¤ë²„ë³€ìˆ˜ëŠ” ì¶”í›„ì— ì¶”ê°€í•˜ëŠ” ë°©í–¥ìœ¼ë¡œ
 }
 
 bool MemberCollection::chechloginMember(std::string id, std::string passwd) {
@@ -12,4 +12,21 @@ bool MemberCollection::chechloginMember(std::string id, std::string passwd) {
 		}
 	}
 	return false;
+}
+
+bool MemberCollection::chechlogoutMember(std::string id) {
+	for (int i = 0; i < memberList.size(); i++) {
+		if (id == memberList[i].id && memberList[i].currentstate == 2) {
+			retrun true;
+		}
+	}
+	return false;
+}
+
+std::string MemberCollection::getlogoutMemberID() {
+	for (int i = 0; i < memberList.size(); i++) {
+		if (memberList[i].currentstate == 2) {
+			retrun memberList[i].id;
+		}
+	}
 }
