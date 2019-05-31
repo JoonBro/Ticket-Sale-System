13a7f91ec1580669a6c1e81ca2f96e1b669cdaa9
wnsgud245 <44499629+wnsgud245@users.noreply.github.com>
2019-05-31 16:01:43 +0900

    Update MemberCollection.h

--- MemberCollection.h
+++ MemberCollection.h
@@ -10,6 +10,8 @@ private:
 public:
 	void createMember(std::string id, std::string passwd, std::string name, std::string ssn, std::string user_type);
 	bool chechloginMember(std::string id, std::string passwd);
+	bool chechlogoutMember(std::string id);
+	std::string getlogoutMemberID()
 };
 
 #endif
