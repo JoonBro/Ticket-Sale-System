0fd93b6982416c3c0c214e13189d96453d66ef2b
yurim022 <yu_rim02@naver.com>
2019-06-01 17:49:13 +0900

    ShowReservedTicket

---
+++ ShowReservedTicket.h
@@ -0,0 +1,20 @@
+#ifndef ShowReservedTicket
+#define ShowReservedTicket 
+#include <string>
+
+class ShowReservedTicket{
+private: 
+	memberCollection = MemberCollection();
+
+public: 
+	ShowReservedTicket();//생성자
+};
+
+class ShowReservedTicketUI{
+public:
+	ShowReservedTicketUI();//생성자
+	void showReservedTicket();
+};
+
+#endif
+
